20ff0c693d933691318addcf598cf81d8f9c8059
p-schulze <pascalschulze@user2394.local>
2015-01-27 09:47:43 +0100

    Setter und Getter für Adresse fertig

--- Baufirma/src/baufirma/Adresse.java
+++ Baufirma/src/baufirma/Adresse.java
@@ -14,10 +14,82 @@ public class Adresse {
     int hausnummer;
     
     Adresse () {
-    
+        
     }
     
-    Adresse (String neueStrasse) {
-    
+    public void Adresse (String neuesLand, String neueStadt, String neuePlz, String neueStrasse, int neueHausnummer) {
+        land = neuesLand;
+        stadt = neueStadt;
+        plz = neuePlz;
+        strasse = neueStrasse;
+        hausnummer = neueHausnummer;
     }
+    
+    public void Adresse (String neuesLand, String neueStadt, String neuePlz, String neueStrasse, int neueHausnummer, String neueTelefonnummer, String neueMailadresse) {
+        land = neuesLand;
+        stadt = neueStadt;
+        plz = neuePlz;
+        strasse = neueStrasse;
+        hausnummer = neueHausnummer;
+        telefonnummer = neueTelefonnummer;
+        mailadresse = neueMailadresse;   
+    }
+    
+    public String getLand () {
+        return land;
+    }
+    
+    public void setLand (String neuesLand) {
+        land = neuesLand;
+    }
+    
+    public String getStadt () {
+        return stadt;
+    }
+    
+    public void setStadt (String neueStadt) {
+        stadt = neueStadt;
+    }
+    
+    public String getPlz () {
+        return plz;
+    }
+    
+    public void setPlz (String neuePlz) {
+        plz = neuePlz;
+    }
+    
+    public String getStrasse () {
+        return strasse;
+    }
+    
+    public void setStrasse (String neueStrasse) {
+        strasse = neueStrasse;
+    }
+    
+    public int getHausnummer () {
+        return hausnummer;
+    }
+    
+    public void setHausnummer (int neueHausnummer) {
+        hausnummer = neueHausnummer;
+    }
+    
+    public String getTelefonnummer () {
+        return telefonnummer;
+    }
+    
+    public void setTelefonnummer (String neueTelefonnummer) {
+        telefonnummer = neueTelefonnummer;
+    }
+    
+    public String getMailadresse () {
+        return mailadresse;
+    }
+    
+    public void setMailadresse (String neueMailadresse) {
+        mailadresse = neueMailadresse;
+    }
+    
+    
 }
