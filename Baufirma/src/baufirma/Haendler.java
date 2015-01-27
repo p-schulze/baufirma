2c6a4d3b347eed277862ffa95dfc0681c95acb2f
p-schulze <pascalschulze@user2394.speedport_w_724v_typ_a_05011602_00_001>
2015-01-27 16:42:04 +0100

    G&S für Klasse Händler angelegt

--- Baufirma/src/baufirma/Haendler.java
+++ Baufirma/src/baufirma/Haendler.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package baufirma;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,4 +12,44 @@ package baufirma;
  */
 public class Haendler {
     
+    String name;
+    Adresse adresse;
+    ArrayList<Material> haendlerMaterialListe;
+    
+    public void Haendler () {
+        
+    }
+    
+    public void Haendler (String neuerName, Adresse neueAdresse) {
+        name = neuerName;
+        adresse = neueAdresse;
+    }
+    
+    public void setName (String neuerName){
+        name = neuerName;
+    }
+    
+    public String getName () {
+        return name;
+    }
+    
+    public void setAdresse(Adresse neueAdresse){
+        adresse = neueAdresse;
+    }
+    
+    public Adresse getAdresse () {
+        return adresse;
+    }
+    
+    public void setHaendlerMaterialListe(ArrayList<Material> neueHaendlerMaterialListe){
+        haendlerMaterialListe = neueHaendlerMaterialListe;
+    }
+    
+    public ArrayList<Material> getHaendlerMaterialListe(){
+        return haendlerMaterialListe;
+    }
+    
+    public void addToHaendlerMaterialListe(Material neuesMaterial){
+        haendlerMaterialListe.add(neuesMaterial);
+    }
 }
