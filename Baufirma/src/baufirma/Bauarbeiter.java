e03038be20e74f15ab56de8cf5e79b3e72db8360
p-schulze <pascalschulze@user2394.local>
2015-01-27 12:50:11 +0100

    Bauarbeiter Konstruktor erstellt

--- Baufirma/src/baufirma/Bauarbeiter.java
+++ Baufirma/src/baufirma/Bauarbeiter.java
@@ -5,12 +5,34 @@
  */
 package baufirma;
 
+import java.util.Date;
+
 /**
  *
  * @author pascalschulze
  */
 public class Bauarbeiter extends Angestellter {
+    String bauarbeiterTyp;
     
+    public void Bauarbeiter () {
+        aktiv = false;
+    }
+    
+    public void Bauarbeiter (String neuerName, String neueBezeichnung, Adresse neueAdresse, Date neuBescheaftigtSeit) {
+        aktiv = false;
+        name = neuerName;
+        bezeichnung = neueBezeichnung;
+        adresse = neueAdresse;
+        bescheaftigtSeit = neuBescheaftigtSeit;
+    }    
+    
+    public void setBauarbeiterTyp (String neuerBauarbeiterTyp) {
+        bauarbeiterTyp = neuerBauarbeiterTyp;
+    }
+    
+    public String getBauarbeiterTyp () {
+        return bauarbeiterTyp;
+    }
     
     
 }
