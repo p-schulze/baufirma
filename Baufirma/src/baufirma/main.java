193b00383bcc036bd60f0883b46b8b05d054a965
p-schulze <pascalschulze@user2394.local>
2015-01-28 10:08:32 +0100

    Synctest1

--- Baufirma/src/baufirma/main.java
+++ Baufirma/src/baufirma/main.java
@@ -15,7 +15,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Hier könnte Pascals Code stehen.
     }
     
 }
